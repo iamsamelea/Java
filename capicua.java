26af755ed5cead4e74fc60c0830d4f60dcf6ef43
iamsamelea <iamsamelea.code@gmail.com>
2022-01-12 10:07:15 +0100

    capicua falta acabar

---
+++ capicua.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+
+
+public class capicua {
+
+    public static void main(String[] args) {
+
+        /* Declarem una variable Scanner */
+        Scanner teclat = new Scanner(System.in);
+
+        int n = teclat.nextInt();
+
+        while (n > 0){
+           
+        }
+    }
+}
